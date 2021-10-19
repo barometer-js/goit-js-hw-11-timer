6782aa8fa2bb537714d957651b3395c83d461d8b
Vitalii Semenenko <SemenenkoSVAT@gmail.com>
2021-10-20 01:59:38 +0300

    created plugin with "class"

--- index.js
+++ index.js
@@ -5,20 +5,55 @@ const refs = {
   secs: document.querySelector('[data-value="secs"]'),
 };
 
-class Timer {
-  constructor(finishDate) {
-    this.finishDate = finishDate;
-    this.intervalId = null;
+const { days, hours, mins, secs } = refs;
+
+class CountdownTimer {
+  constructor({ selector, targetDate, markup }) {
+    this.selector = document.querySelector(selector);
+    this.targetDate = targetDate;
+    this.markup = markup;
     this.deltaTime = 0;
+  }
+  start() {
+    setInterval(() => {
+      let currentTime = Date.now();
+      this.deltaTime = this.targetDate - currentTime;
+      console.log(this.targetDate);
+      const days = this.pad(Math.floor(this.deltaTime / (1000 * 60 * 60 * 24)));
+      const hours = this.pad(
+        Math.floor((this.deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      );
+      const mins = this.pad(
+        Math.floor((this.deltaTime % (1000 * 60 * 60)) / (1000 * 60))
+      );
+      const secs = this.pad(Math.floor((this.deltaTime % (1000 * 60)) / 1000));
+      console.log(days, hours, mins, secs);
+      this.updateClockface(days, hours, mins, secs);
+      this.style();
+    }, 1000);
+  }
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+  updateClockface(days, hours, mins, secs) {
+    this.markup.days.textContent = `${days}`;
+    this.markup.hours.textContent = `${hours}`;
+    this.markup.mins.textContent = `${mins}`;
+    this.markup.secs.textContent = `${secs}`;
+  }
+  style() {
+    this.selector.style.fontFamily = "sans-serif";
+    this.selector.style.display = "flex";
+    this.selector.style.alignItems = "center";
+    this.selector.style.justifyContent = "center";
+    this.selector.style.fontSize = "18px";
   }
 }
 
-new CountdownTimer({
+const myBirthdayCountdown = new CountdownTimer({
   selector: "#timer-1",
-  targetDate: new Date("Jul 17, 2019"),
+  targetDate: new Date("May 11, 2022"),
+  markup: { days, hours, mins, secs },
 });
 
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+myBirthdayCountdown.start();
